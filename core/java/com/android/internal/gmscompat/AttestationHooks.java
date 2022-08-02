93d17388e36d3d58ba552483f7da1530ac042809
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2022-08-02 19:04:33 +0530

    gmscompat/attestation: Set PRODUCT_STOCK_FINGERPRINT to old angler one
     * Reference:
       https://github.com/kdrag0n/safetynet-fix/pull/207
    
    Change-Id: I01981656dd796c91a8fb488a51b862eef72b4c71

--- core/java/com/android/internal/gmscompat/AttestationHooks.java
+++ core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -32,7 +32,7 @@ public final class AttestationHooks {
     private static final String PROCESS_UNSTABLE = "com.google.android.gms.unstable";
 
     private static final String PRODUCT_STOCK_FINGERPRINT =
-            SystemProperties.get("ro.build.stock_fingerprint");
+            "google/angler/angler:6.0/MDB08L/2343525:user/release-keys";
 
     private static volatile boolean sIsGms = false;
 
